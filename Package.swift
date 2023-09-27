60ba2a8b2e43484c1390db9549cd12bb4c6b01d4
Pedro Loureiro <pedro.loureiro@diffusiondata.com>
2023-09-27 07:47:37 +0100

    Updated Diffusion Swift Package to 6.9.0

--- Package.swift
+++ Package.swift
@@ -8,10 +8,15 @@ let package = Package(
     products: [
         .library(
             name: "Diffusion",
-            targets: ["Diffusion"]),
+            targets: [ "Diffusion" ]
+        ),
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "Diffusion", path: "./Sources/Diffusion.xcframework")
+        .binaryTarget(
+            name: "Diffusion",
+            url: "https://github.com/diffusiondata/diffusion-sdk-apple/raw/6.9.0/Diffusion.xcframework.zip",
+            checksum: "0597f564169e0279f9a6b72939a90e7790e877d05386f9599a5a0d1995cec895"
+        )
     ]
 )
