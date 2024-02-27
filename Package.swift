ef0804916cd9946c9f106524960abad82930b578
GitLab <pedro.loureiro@diffusiondata.com>
2024-02-27 13:33:23 +0000

    Updated Diffusion Swift Package to 6.9.5

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "Diffusion",
             url: "https://github.com/diffusiondata/diffusion-sdk-apple/raw/6.9.5/Diffusion.xcframework.zip",
-            checksum: "2a010fb6c069c1b3c3b8c374f41ff27df35ff3bf6b0405547fee5558640a8084"
+            checksum: "63db91c83407acedcb3ac637e839a7aee7d69c29b4ffe304259888456ea5b2b6"
         )
     ]
 )
